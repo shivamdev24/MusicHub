bb5c4d3d09b0d3fc6deaacd98c76f02c1dabc2ae
Shivam Sharma <148194095+DevShivam2402@users.noreply.github.com>
2024-01-08 20:46:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-8xl text-gray-300 h-screen bg-slate-800 font-bold text-center pt-80">This is MarKetHub</h1>
+    </>
+  );
+}
